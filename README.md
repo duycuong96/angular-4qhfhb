334d37f130abe085db46d543b26822409d18dc10
duycuong96 <vuduycuong996@gmail.com>
2020-03-21 14:35:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-4qhfhb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-4qhfhb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router'
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+const appRoutes: Routes = [
+  {
+    
+  }
+]
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
