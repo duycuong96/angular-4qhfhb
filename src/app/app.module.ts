b852173f7acab5bc4488ae6722dbac04ecddbed0
duycuong96 <vuduycuong996@gmail.com>
2020-03-22 14:17:16 +0700

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,39 @@ import { HelloComponent } from './hello.component';
 import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
+import { CrisisListComponent } from './crisis-center/crisis-list/crisis-list.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {
     path: 'crisis-center',
-  
+    component: CrisisListComponent
+  },
+  {
+    path: 'hero/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'heroes',
+    component: HeroListComponent, 
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ]
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
+  ],
   declarations: [ AppComponent, HelloComponent, CrisisCenterComponent, HeroesComponent, HeroListComponent ],
   bootstrap:    [ AppComponent ]
 })
